f316eed0a0ce3029d1e96243c16bd8b7831191e0
Safidy-Mahefa <safidymahefa03@gmail.com>
2026-02-10 18:02:43 +0200

    first commit :)

---
+++ HelloJava/src/com/myapp/Main.java
@@ -0,0 +1,9 @@
+package com.myapp; //le package où ce trouve ce fichier
+
+public class Main{
+    public static void main(String[] args){
+        String nom = "Safidy";
+        int age = 50;
+        System.out.println("Hello java ! Je m'appelle :  "+nom+" et j'ai : " +age+" ans ");
+    }
+}
